e9eb31d7c4a615d62ab94c72d79175f5b19e2700
anniasebold <anniasebold3.0@gmail.com>
2022-03-31 20:40:09 -0300

    Post Contacts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,39 @@
 const express = require('express')
+const { v4: uuidv4 } = require("uuid")
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
+const customers = []
+
+/*
+  cpf - string
+  name - string
+  id - uuid
+  statement []
+
+*/
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+  
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists" })
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: []
+  })
+
+  console.log(customers);
+  return response.status(201).send();
+})
 
 app.listen(3000, console.log('Server started at http://localhost:3000'));
